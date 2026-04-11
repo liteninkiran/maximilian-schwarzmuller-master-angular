c8541590aeb9aee59199db4178d9eb56a952cc58
Kiran Anand <liteninkiran@hotmail.com>
2026-04-11 21:44:24 +0100

    027 - Managing State & Changing Data

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -10,13 +10,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   imports: [],
 })
 export class User {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = DUMMY_USERS[randomIndex()];
 
   get imagePath() {
     return `users/${this.selectedUser.avatar}`;
   }
 
   onSelectUser() {
-    console.log('Clicked!');
+    this.selectedUser = DUMMY_USERS[randomIndex()];
   }
 }
