fe36fa68a9fc22925b6b7ff3dce687d5caec4d78
Kiran Anand <liteninkiran@hotmail.com>
2026-04-11 21:26:25 +0100

    018 - Managing & Creating Components with the Angular CLI

---
+++ src/app/user/user.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.html',
+  styleUrl: './user.css',
+  imports: [],
+})
+export class User {}
