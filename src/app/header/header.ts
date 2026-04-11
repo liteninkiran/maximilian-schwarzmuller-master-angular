904c991f6d1922cbb50b2ababd2f7ad0a6fe847f
Kiran Anand <liteninkiran@hotmail.com>
2026-04-11 20:57:56 +0100

    017 - Styling the Header Component & Adding An Image

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
   templateUrl: './header.html',
+  styleUrls: ['./header.css'],
+  standalone: true,
 })
 export class Header {}
