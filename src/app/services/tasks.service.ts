0a6c003f8050fa8d392b02aafe3c416ca0a412e6
Kiran Anand <liteninkiran@hotmail.com>
2026-04-12 20:12:42 +0100

    063 - Using localStorage for Data Storage

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,10 +1,18 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { type NewTaskData, type TaskObject } from '../models/task.model';
 import { TASKS } from '../dummy-data';
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
-  tasks = signal(TASKS);
+  private stored = localStorage.getItem('tasks');
+  private tasks = signal<TaskObject[]>(this.stored ? JSON.parse(this.stored) : TASKS);
+
+  constructor() {
+    effect(() => {
+      const tasksJson = JSON.stringify(this.tasks());
+      localStorage.setItem('tasks', tasksJson);
+    });
+  }
 
   getUserTasks(userId: string) {
     return this.tasks().filter((task) => task.userId === userId);
@@ -12,13 +20,13 @@ export class TasksService {
 
   addTask(inputs: NewTaskData, userId: string) {
     const newTask = this.createTaskObject(inputs, userId);
-    this.tasks.set([...this.tasks(), newTask]);
+    this.tasks.update((tasks) => [...tasks, newTask]);
   }
 
   removeTask(id: string) {
     const filterFn = (task: TaskObject) => task.id !== id;
-    const newTasks = this.tasks().filter(filterFn);
-    this.tasks.set(newTasks);
+    const updateFn = (tasks: TaskObject[]) => tasks.filter(filterFn);
+    this.tasks.update(updateFn);
   }
 
   private maxTask() {
