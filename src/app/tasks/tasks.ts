8b64f75af8ffab6ae33221b6de2ea1e2fe6cde59
Kiran Anand <liteninkiran@hotmail.com>
2026-04-12 16:38:14 +0100

    051 - Creating & Conditionally Rendering Another Component

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -3,21 +3,27 @@ import { Task } from './task/task';
 import { UserObject } from '../models/user.model';
 import { TASKS } from '../dummy-data';
 import { TaskObject } from '../models/task.model';
+import { NewTask } from './new-task/new-task';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
-  imports: [Task],
+  imports: [Task, NewTask],
 })
 export class Tasks {
   tasks = signal(TASKS);
   user = input.required<UserObject>();
+  isAddingTask = signal(false);
   userTasks = computed(() => this.tasks().filter((task) => task.userId === this.user().id));
 
   onCompleteTask(id: string) {
     const filterFn = (task: TaskObject) => task.id !== id;
     const newTasks = this.tasks().filter(filterFn);
     this.tasks.set(newTasks);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask.set(true);
   }
 }
