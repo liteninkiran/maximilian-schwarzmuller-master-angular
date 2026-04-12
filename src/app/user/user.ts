5a1cfeadc3ca0d2fa7dbae459ef6cf13552123e9
Kiran Anand <liteninkiran@hotmail.com>
2026-04-12 16:01:33 +0100

    049 - Dynamic CSS Styling with Class Bindings

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,14 +1,16 @@
 import { Component, computed, input, output } from '@angular/core';
 import { UserObject } from '../models/user.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.html',
   styleUrl: './user.css',
-  imports: [],
+  imports: [CommonModule],
 })
 export class User {
   user = input.required<UserObject>();
+  isSelected = input.required<boolean>();
   select = output<string>();
   imagePath = computed(() => `users/${this.user().avatar}`);
 
