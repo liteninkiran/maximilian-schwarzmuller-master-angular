9cb39638ee3cfe6b0db15a07470020a5017e06a4
Kiran Anand <liteninkiran@hotmail.com>
2026-04-12 18:56:59 +0100

    058 - Transforming Template Data with Pipes

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -1,12 +1,13 @@
 import { Component, input, output } from '@angular/core';
 import { TaskObject } from '../../models/task.model';
 import { CardComponent } from '../../shared/card/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.html',
   styleUrl: './task.css',
-  imports: [CardComponent],
+  imports: [CardComponent, CommonModule],
 })
 export class Task {
   task = input.required<TaskObject>();
