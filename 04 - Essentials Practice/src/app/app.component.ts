99cf9efc7e44d5d4aaf501c00a1cb7c313711beb
Kiran Anand <liteninkiran@hotmail.com>
2026-04-25 16:34:42 +0100

    077 - Adding a Header Component With An Image

--- 04 - Essentials Practice/src/app/app.component.ts
+++ 04 - Essentials Practice/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
+  imports: [HeaderComponent],
 })
 export class AppComponent {}
