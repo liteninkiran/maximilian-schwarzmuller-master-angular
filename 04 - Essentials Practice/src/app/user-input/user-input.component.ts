adbebe1709856f95fd6f629c704c07ea0f669f91
Kiran Anand <liteninkiran@hotmail.com>
2026-04-25 16:40:01 +0100

    080 - Extracting Values with Two-Way-Binding

--- 04 - Essentials Practice/src/app/user-input/user-input.component.ts
+++ 04 - Essentials Practice/src/app/user-input/user-input.component.ts
@@ -9,7 +9,15 @@ import { FormsModule } from '@angular/forms';
   imports: [FormsModule],
 })
 export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
   onSubmit() {
     console.log('SUBMITTED!');
+    console.log(this.enteredInitialInvestment);
+    console.log(this.enteredAnnualInvestment);
+    console.log(this.enteredExpectedReturn);
+    console.log(this.enteredDuration);
   }
 }
