c575fc41e9b6fcb42f97e40e8563dcc5a7912cae
Kiran Anand <liteninkiran@hotmail.com>
2026-04-26 21:20:48 +0100

    105 - A Possible, But Not Ideal Way Of Extending Built-in Elements

---
+++ 06 - Component Deep Dive/src/app/shared/button/button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'button[appButton]',
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
+  standalone: true,
+  imports: [],
+})
+export class ButtonComponent {}
