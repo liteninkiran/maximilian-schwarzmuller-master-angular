04ee29c731a7dab2130bf1a1edaf69b307d4b13a
Kiran Anand <liteninkiran@hotmail.com>
2026-04-26 20:54:39 +0100

    101 - Component Inputs: Repetition

--- 06 - Component Deep Dive/src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ 06 - Component Deep Dive/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+
+export type Image = {
+  src: string;
+  alt: string;
+};
 
 @Component({
   selector: 'app-dashboard-item',
+  templateUrl: './dashboard-item.component.html',
+  styleUrl: './dashboard-item.component.css',
   standalone: true,
   imports: [],
-  templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
 })
 export class DashboardItemComponent {
-
+  image = input.required<Image>();
+  title = input.required<string>();
 }
--- 06 - Component Deep Dive/src/app/dashboard/server-status/server-status.component.ts
+++ 06 - Component Deep Dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {
+  DashboardItemComponent,
+  Image,
+} from '../dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-server-status',
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
   standalone: true,
-  imports: [],
+  imports: [DashboardItemComponent],
 })
 export class ServerStatusComponent {
   currentStatus = 'online';
+  image: Image = {
+    src: 'status.png',
+    alt: 'A signal symbol',
+  };
 }
