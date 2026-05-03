374179ed63ec146ccd420faddb8ac60846c94931
Kiran Anand <liteninkiran@hotmail.com>
2026-05-03 17:34:40 +0100

    122 - Manipulating State & Using Literal Values

--- 06 - Component Deep Dive/src/app/dashboard/server-status/server-status.component.ts
+++ 06 - Component Deep Dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+type Status = 'online' | 'offline' | 'unknown';
+
 @Component({
   selector: 'app-server-status',
   templateUrl: './server-status.component.html',
@@ -9,5 +11,18 @@ import { Component } from '@angular/core';
   imports: [CommonModule],
 })
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  currentStatus: Status = 'online';
+
+  constructor() {
+    setInterval(() => {
+      const rnd = Math.random();
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
