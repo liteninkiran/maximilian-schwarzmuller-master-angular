d2aa10f4060b89dc8873614f0c08d2a4172443a7
Kiran Anand <liteninkiran@hotmail.com>
2026-05-03 18:37:55 +0100

    128 - Handling Form Submissions: Repetition

--- 06 - Component Deep Dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06 - Component Deep Dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  onSubmit() {
+    console.log('Submitted');
+  }
+}
