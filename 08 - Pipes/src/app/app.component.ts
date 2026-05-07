a18927df2fefa4c2cb0629bd51ec3f0bf768c48f
Kiran Anand <liteninkiran@hotmail.com>
2026-05-07 14:53:53 +0100

    165 - Using Built-in Pipes

--- 08 - Pipes/src/app/app.component.ts
+++ 08 - Pipes/src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   templateUrl: './app.component.html',
+  standalone: true,
+  imports: [CommonModule],
 })
 export class AppComponent {
   currentDate = new Date();
--- 08 - Pipes/src/main.ts
+++ 08 - Pipes/src/main.ts
@@ -1,5 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-
 import { AppComponent } from './app/app.component';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeGb from '@angular/common/locales/en-GB';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+registerLocaleData(localeGb);
+
+bootstrapApplication(AppComponent, {
+  providers: [{ provide: LOCALE_ID, useValue: 'en-GB' }],
+}).catch((err) => console.error(err));
