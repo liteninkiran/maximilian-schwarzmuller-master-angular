bf6f63a3441b3b9131b6258b9a019a7e40c0e9a0
Kiran Anand <liteninkiran@hotmail.com>
2026-05-07 20:16:24 +0100

    181 - Outsourcing & Reusing Logic with Services (Refactor)

--- 09 - Services and DI/src/app/tasks/task.model.ts
+++ 09 - Services and DI/src/app/tasks/task.model.ts
@@ -1,7 +1,7 @@
 export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
 
 export interface Task {
-  id: string;
+  id: `${string}-${string}-${string}-${string}-${string}`;
   title: string;
   description: string;
   status: TaskStatus;
@@ -9,19 +9,19 @@ export interface Task {
 
 export const EXAMPLE_TASKS: Task[] = [
   {
-    id: '1',
+    id: '1-1-1-1-1',
     title: 'Task 1',
     description: 'Task 1 - description',
     status: 'DONE',
   },
   {
-    id: '2',
+    id: '1-1-1-1-2',
     title: 'Task 2',
     description: 'Task 2 - description',
     status: 'IN_PROGRESS',
   },
   {
-    id: '3',
+    id: '1-1-1-1-3',
     title: 'Task 3',
     description: 'Task 3 - description',
     status: 'OPEN',
--- 09 - Services and DI/src/app/tasks/tasks.service.ts
+++ 09 - Services and DI/src/app/tasks/tasks.service.ts
@@ -2,28 +2,7 @@ import { Injectable, signal } from '@angular/core';
 import { EXAMPLE_TASKS, Task, TaskStatus } from './task.model';
 
 type NewTaskData = Omit<Task, 'id' | 'status'>;
-
-type Config = {
-  status: TaskStatus;
-  task: Task;
-  taskId?: string;
-};
-
-const taskFromData = (taskData: NewTaskData) =>
-  ({
-    ...taskData,
-    id: crypto.randomUUID(),
-    status: 'OPEN',
-  }) as Task;
-const addTask = (oldTasks: Task[], taskData: NewTaskData) => [
-  ...oldTasks,
-  taskFromData(taskData),
-];
-const updatedTaskStatus = ({ task, status }: Config) => ({ ...task, status });
-const updateTaskStatus = ({ task, status, taskId }: Config) =>
-  task.id === taskId ? updatedTaskStatus({ task, status }) : task;
-const updateStatus = (oldTasks: Task[], taskId: string, status: TaskStatus) =>
-  oldTasks.map((task: Task) => updateTaskStatus({ task, taskId, status }));
+type TaskIdentity = Pick<Task, 'id' | 'status'>;
 
 @Injectable({
   providedIn: 'root',
@@ -33,11 +12,24 @@ export class TasksService {
 
   getAllTasks = this.tasks.asReadonly();
 
-  addTask(taskData: NewTaskData) {
-    this.tasks.update((oldTasks) => addTask(oldTasks, taskData));
+  addTask(data: NewTaskData) {
+    type Config = { tasks: Task[]; data: NewTaskData };
+    const getProps = () =>
+      ({ id: crypto.randomUUID(), status: 'OPEN' }) as TaskIdentity;
+    const taskFromData = (data: NewTaskData) =>
+      ({ ...data, ...getProps() }) as Task;
+    const addNewTask = ({ tasks, data }: Config) => [
+      ...tasks,
+      taskFromData(data),
+    ];
+    this.tasks.update((tasks) => addNewTask({ tasks, data }));
   }
 
   updateTaskStatus(taskId: string, status: TaskStatus) {
-    this.tasks.update((oldTasks) => updateStatus(oldTasks, taskId, status));
+    const update = (task: Task) => ({ ...task, status });
+    const mapTask = (task: Task) => (task.id === taskId ? update(task) : task);
+    const updateTask = (tasks: Task[]) => tasks.map(mapTask);
+
+    this.tasks.update(updateTask);
   }
 }
